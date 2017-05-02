d1953e48092059cf1b62ab9df028b3b0990519b3
Eric Mann <eric@tozny.com>
2017-05-02 14:07:30 -0700

    Remove unnecessary Events URL mappings

--- client.go
+++ client.go
@@ -38,29 +38,27 @@ type akCacheKey struct {
 
 // ClientOpts contains options for configuring an E3DB client.
 type ClientOpts struct {
-	ClientID      string
-	APIKeyID      string
-	APISecret     string
-	PublicKey     publicKey
-	PrivateKey    privateKey
-	APIBaseURL    string
-	AuthBaseURL   string
-	EventsBaseURL string
-	Logging       bool
+	ClientID    string
+	APIKeyID    string
+	APISecret   string
+	PublicKey   publicKey
+	PrivateKey  privateKey
+	APIBaseURL  string
+	AuthBaseURL string
+	Logging     bool
 }
 
 // Client is an authenticated connection to the E3DB service, providing
 // access to end-to-end encrypted data stored in the database.
 type Client struct {
-	ClientID      string
-	APIKeyID      string
-	APISecret     string
-	PublicKey     publicKey
-	PrivateKey    privateKey
-	APIBaseURL    string
-	AuthBaseURL   string
-	EventsBaseURL string
-	Logging       bool
+	ClientID    string
+	APIKeyID    string
+	APISecret   string
+	PublicKey   publicKey
+	PrivateKey  privateKey
+	APIBaseURL  string
+	AuthBaseURL string
+	Logging     bool
 
 	httpClient *http.Client
 	akCache    map[akCacheKey]secretKey
